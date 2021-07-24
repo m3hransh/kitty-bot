39ceb068267bb428ae9a78e0024ecb629f718dcd
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-07-24 16:57:39 +0430

    added discord config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello');
+import { Client } from 'discord.js';
+
+(async () => {
+  const bot = new Client();
+  bot.on('ready', () => console.log('Connected to Discord!'));
+  await bot.login(process.env.BOT_TOKEN);
+})();
